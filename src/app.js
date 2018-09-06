da61f30584618513b82d74c02fc5bd4ce7c9d6b6
woffles <wolfgang.truong@gmail.com>
2018-09-06 15:10:11 -0700

    about to deploy

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 
 module.exports = app;
-
+const appConfig
 const routeConfig = require("./config/route-config.js");
 
+appConfig.init();
 routeConfig.init(app);
